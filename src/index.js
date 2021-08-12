0d7631021d0760218fce80708534fdf3104029d4
Alexei-T1 <turencko.alexei@yandex.ru>
2021-08-12 14:02:52 +0400

    feat: add solution for this task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, brackets) {
+  
+    let checkStr = true;
+    while (checkStr) {
+      let newStr = str;
+
+      if (brackets[0][0].charCodeAt(0) > 47 && brackets[0][0].charCodeAt(0) < 58) {
+          for (let arrBr of brackets) {
+              let first = arrBr[0];
+              let second = arrBr[1];
+              let reg = new RegExp(`${first}` + `${second}`, "g");
+              newStr = newStr.replace(reg, '');
+          }
+      }
+
+      else {
+          for (let arrBr of brackets) {
+              let first = arrBr[0];
+              let second = arrBr[1];
+              let reg = new RegExp(`\\${first}` + `\\${second}`, "g");
+              newStr = newStr.replace(reg, '');
+          }
+      }
+  
+      if (newStr == str) break;
+      str = newStr;
+
+  }
+  return (str) ? false: true;
 }
